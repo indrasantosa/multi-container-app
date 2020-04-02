9e625f107223342dad313d341668801bc65b9973
Indra Santosa <indrasantosa90@gmail.com>
2020-04-03 02:47:16 +0800

    Add docker compose

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const pgClient = new Pool({
   user: keys.pgUser,
   host: keys.pgHost,
   database: keys.pgDatabase,
-  password: keys.pgDatabase,
+  password: keys.pgPassword,
   port: keys.pgPort
 });
 
