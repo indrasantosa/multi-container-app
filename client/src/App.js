0468411e21fa2f6ba6e43e7f43e4a32b1ed60375
Indra Santosa <indrasantosa90@gmail.com>
2020-04-03 02:01:27 +0800

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Fib from './Fib';
+import OtherPage from './OtherPage';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <Link to={'/'}>Home</Link>
+        <Link to={'/otherpage'}>Other Page</Link>
+        <Route exact path={'/'} component={Fib} />
+        <Route path={'/otherpage'} component={OtherPage} />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
